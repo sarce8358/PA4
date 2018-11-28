bce591b28e848d0036793ffc623032bfcff8a7b0
sarce8358 <45366541+sarce8358@users.noreply.github.com>
2018-11-27 21:52:55 -0600

    Update TeamStats.java

--- TeamStats.java
+++ TeamStats.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class TeamStats
 {
   /**
    * create 1D array at the class level called myTeams at the class level as a null array the type will be Team
    */
-private Team[] myTeams = null;
+  private static Scanner input = new Scanner(System.in);
+  private Team[] myTeams = null;
   
   //-------------------------------------declaring methods
   
@@ -15,6 +17,18 @@ private Team[] myTeams = null;
      * 
      * if answer is "No" then print message "Thank you! Exiting program."
      */
+    System.out.printf("%nDo you want to track the performance of the athletic "
+                        + "teams at your school?  Enter 'Y' or 'N':  ");
+    char ans = input.nextLine().charAt(0);
+    
+    if(Character.toUpperCase(ans) == 'Y') {
+      processWinsLosses();
+    } else if (Character.toUpperCase(ans) == 'N') {
+System.out.printf("Thank you! Exiting program.");
+    
+    }
+    
+    
     
   }//END start()
   
@@ -24,8 +38,14 @@ private Team[] myTeams = null;
      * prmot user on number of teams that they would like to track. this number will be used to make the size of the 
      * myTeams array.
      */
-  }//END processWinsLosses()
+    System.out.printf("%nWhat is the name of your school?");
+    String name = input.nextLine();
     
+    System.out.printf("%nHow many athletic teams do you have at your school?");
+    //input.nextLine();
+    
+  }//END processWinsLosses()
+  
   public void displayTeamStats()
   {
     /**
