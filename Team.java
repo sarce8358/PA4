4b6309710f3fa6342d2226a218e19f2e88639945
sarce8358 <45366541+sarce8358@users.noreply.github.com>
2018-11-27 23:13:51 -0600

    Update Team.java

--- Team.java
+++ Team.java
@@ -3,46 +3,57 @@ import java.util.Scanner;
 public class Team
 {
    private static Scanner input = new Scanner(System.in);
+   private static int totalGame = 0;
+   private static int wins = 0;
+   private static String teamType = " ";
+   private static String coachName = " ";
+   
   
   //-------------------------------------declaring methods
   
   public void setTeam()
   {
     System.out.printf("%nEnter the team type:  ");
-    String teamType = input.nextLine();
+    teamType = input.nextLine();
   }//END setTeam()
   
   public void setCoach()
   {
     System.out.printf("%nEnter the name of the coach: ");
-    String coachName = input.nextLine();
+    coachName = input.nextLine();
   }//END setCoach()
   
   public void setTotalGames()
   {
     System.out.printf("%nEnter the total number of games played for the "
-                     + "%s team: ", );
+                     + "%s team: ", getTeam());
+    totalGame = input.nextInt();
+    input.nextLine();
+    
   }//END setTotalGames()
   
   public void setWins()
   {
-    
+   System.out.printf("%nEnter the number of wins for the %s team: "
+                       , getTeam()); 
+   wins = input.nextInt();
+   input.nextLine();
   }//END setWins()
   
   
-  public void getTeam(){
-    
-  }
+  public String getTeam(){
+    return teamType;
+  }//END getTeam()
   
-  public void getCoach(){
-    
-  }
+  public String getCoach(){
+    return coachName;
+  }//END getCoach()
   
-  public void getTotalGames(){
+  public int getTotalGames(){
+  return totalGame;
+  }//END getTotalGames()
   
-  }
-  
-  public void setWins(){
-    
-  }
+  public int getWins(){
+    return wins;
+  }//END getWins()
 }//END class
