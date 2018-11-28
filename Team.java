da6845d1d1711d6fb321308f3cb8c36e3bc89094
QualityGarbage <45342491+QualityGarbage@users.noreply.github.com>
2018-11-27 17:51:22 -0800

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,28 @@
+public class Team
+{
+  public static void main(String [] args)
+  {
+  }//END main
+  
+  //-------------------------------------declaring methods
+  
+  public void setTeam()
+  {
+    
+  }//END setTeam()
+  
+  public void setCoach()
+  {
+    
+  }//EDN setCoach()
+  
+  public void setTotalGames()
+  {
+    
+  }//END setTotalGames()
+  
+  public void setWins()
+  {
+    
+  }//EDN setWins()
+}//END class
---
+++ TeamStats.java
@@ -0,0 +1,42 @@
+public class TeamStats
+{
+  /**
+   * create 1D array at the class level called myTeams at the class level as a null array the type will be Team
+   */
+  public static void main(String [] args)
+  {
+    /**
+     * 
+     */
+   
+  }//END main
+  
+  //-------------------------------------declaring methods
+  
+  
+  public void start()
+  {
+    /**
+     * if the answer is "Yes" to tracking team performance then call processWinsLosses() and displayTeamStats()
+     * 
+     * if answer is "No" then print message "Thank you! Exiting program."
+     */
+    
+  }//END start()
+  
+  public void processWinsLosses()
+  {
+    /**
+     * prmot user on number of teams that they would like to track. this number will be used to make the size of the 
+     * myTeams array.
+     */
+  }//END processWinsLosses()
+    
+  public void displayTeamStats()
+  {
+    /**
+     * see sample output on instructions
+     */
+  }//END displayTeamStats()
+  
+}//END class
