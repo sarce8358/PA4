66946f9a6575336719af8001a2ab1982d1733983
sarce8358 <45366541+sarce8358@users.noreply.github.com>
2018-11-27 21:34:12 -0600

    Update ArceSharaf001PA4.java

--- ArceSharaf001PA4.java
+++ ArceSharaf001PA4.java
@@ -5,13 +5,20 @@
  * 
  * PROGRAM PURPOSE:
  */
-
+import java.util.Scanner;
 public class ArceSharaf001PA4
 {
+  private static Scanner input = new Scanner(System.in);
+  
   public static void main(String[] args)
   {
     //CREATE AN OBJECT OF TeamStats to call the method(s)
     //that kick starts the program.
+    System.out.printf("Do you want to track the performance of the athletic "
+                        + "teams at your school?  Enter 'Y' or 'N':  ");
+    
+    TeamStats teamStats = new TeamStats();
+    teamStats.start();
     
     System.exit(0);
   }//END main()
